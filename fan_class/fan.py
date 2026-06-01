352a494657f7fd448f11f3eb962f776bd0f840c1
RvJames26 <rovelljamesmariano05@gmail.com>
2026-06-01 21:15:02 +0800

    set up the accessor and mutator

--- fan_class/fan.py
+++ fan_class/fan.py
@@ -6,3 +6,15 @@ class Fan:
     def __init__(self, fan_speed, fan_status=False):
         self.__fan_speed = fan_speed
         self.__fan_status = fan_status
+
+    def get_fan_speed(self):
+        return self.__fan_speed
+
+    def get_fan_speed(self, fan_speed):
+        self.__fan_speed = fan_speed
+
+    def get_fan_status(self):
+        return self.__fan_status
+
+    def set_fan_status(self, fan_status):
+
