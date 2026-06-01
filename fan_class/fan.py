dacae10a7120ae58db6d609a7adc76415dd9af42
RvJames26 <rovelljamesmariano05@gmail.com>
2026-06-01 20:54:09 +0800

    set up the def member

--- fan_class/fan.py
+++ fan_class/fan.py
@@ -1,4 +1,6 @@
 class Fan:
 
-    def __init__(self, name):
+    def __init__(self, name, fan_speed, fan_radius, fan_color):
         self.name = name
+        self.fan_speed = fan_speed
+        self.fan_radius = fan_radius
