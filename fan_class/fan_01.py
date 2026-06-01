9708ebc4f723b03d3581152e2e313f415912719d
RvJames26 <rovelljamesmariano05@gmail.com>
2026-06-01 21:39:33 +0800

    set up the radius and color

--- fan_class/fan_01.py
+++ fan_class/fan_01.py
@@ -3,9 +3,11 @@ class FanOne:
     medium = 2
     fast = 3
 
-    def __init__(self, fan_speed=3, fan_status=False):
+    def __init__(self, fan_speed=1, fan_status=False, fan_radius=5, fan_color="yellow"):
         self.__fan_speed = fan_speed
         self.__fan_status = fan_status
+        self.__fan_radius = fan_radius
+        self.__fan_color = fan_color
 
     def get_fan_speed(self):
         return self.__fan_speed
@@ -18,6 +20,18 @@ class FanOne:
 
     def set_fan_status(self, fan_status):
         self.__fan_status = fan_status
+
+    def get_fan_radius(self):
+        return self.__fan_radius
+
+    def set_fan_radius(self, fan_radius):
+        self.__fan_radius = fan_radius
+
+    def get_fan_color(self):
+        return self.__fan_color
+
+    def set_fan_color(self, fan_color):
+        self.__fan_color = fan_color
 
 my_fan = FanOne()
 my_fan.set_fan_status(True)
@@ -35,4 +49,7 @@ elif current_speed == 2:
     print(f"Fan speed: MEDIUM")
 elif current_speed == 3:
     print(f"Fan speed: FAST")
+
+print(f"Fan radius: {my_fan.get_fan_radius()}")
+print(f"Fan color: {my_fan.get_fan_color()}")
 
