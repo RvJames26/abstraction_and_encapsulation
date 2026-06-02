42bc286d715231390e8b4b5e4064d33eaa113c67
RvJames26 <rovelljamesmariano05@gmail.com>
2026-06-02 21:25:42 +0800

    added method for getting the attribute

--- car_class/car.py
+++ car_class/car.py
@@ -1,8 +1,15 @@
 class Car:
 
-    def __init__(self, year_model, make, speed=0):
+    def __init__(self, year_model, make, speed):
         self.__year_model = year_model
         self.__make = make
-        self.__speed = speed
+        self.__speed = 0
 
+    def get_year_model(self):
+        return self.__year_model
 
+    def get_make(self):
+        return self.__make
+
+    def get_speed(self):
+        return self.__speed
