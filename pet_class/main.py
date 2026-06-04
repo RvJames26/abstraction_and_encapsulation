ffc0031ec3afa23f5a50b09ec1a9f999d1787094
RvJames26 <rovelljamesmariano05@gmail.com>
2026-06-04 23:12:38 +0800

    added exception handling to the pets age

--- pet_class/main.py
+++ pet_class/main.py
@@ -1,11 +1,20 @@
 from pet import Pet
 
 input_name = input("Enter your pet's name:")
-input_age = input("Enter pet's age:")
+
+while True:
+    try:
+        input_age = int(input("Enter pet's age:"))
+        break
+    except ValueError:
+        print(f"Age should be a number")
+    else:
+        continue
+
 input_animal_type = input("Enter pet's animal type:")
 
 my_pet = Pet(input_name, input_age, input_animal_type)
 
-
+print(f"Your pet is {my_pet.get_name()}")
 
 Pet()
