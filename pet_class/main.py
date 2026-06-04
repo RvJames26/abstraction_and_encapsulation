66aa52583c719136bdc78da9f9fbb07c8f70543d
RvJames26 <rovelljamesmariano05@gmail.com>
2026-06-04 23:02:13 +0800

    created the input and also the object's variable

---
+++ pet_class/main.py
@@ -0,0 +1,11 @@
+from pet import Pet
+
+input_name = input("Enter your pet's name:")
+input_age = input("Enter pet's age:")
+input_animal_type = input("Enter pet's animal type:")
+
+my_pet = Pet(input_name, input_age, input_animal_type)
+
+
+
+Pet()
